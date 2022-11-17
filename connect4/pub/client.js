61d7e10828122b1cd9ba1bdee5429d82d1ffbda1
Zach <bowerszach5@gmail.com>
2022-11-17 00:34:39 -0500

    Created grid and started on addChip function.

--- connect4/pub/client.js
+++ connect4/pub/client.js
@@ -1,19 +1,34 @@
 let myApp = Vue.createApp({
 	data() {
 		return {
-            grid: [
-				[{id: 1, color: "null", row: 6}, 2, 3, 4, 5, 6, 7],
-				[8, 9, 10, 11, 12, 13, 14],
-				[15, 16, 17, 18, 19, 20, 21],
-				[22, 23, 24, 25, 26, 27, 28],
-				[29, 30, 31, 32, 33, 34, 35],
-				[36, 37, 38, 39, 40, 41, 42],
+			grid: [
+				{id: 1, color: "null", row: 6}, {id: 2, color: "null", row: 6}, {id: 3, color: "null", row: 6}, {id: 4, color: "null", row: 6}, {id: 5, color: "null", row: 6}, {id: 6, color: "null", row: 6}, {id: 7, color: "null", row: 6},
+				{id: 8, color: "null", row: 5}, {id: 9, color: "null", row: 5}, {id: 10, color: "null", row: 5}, {id: 11, color: "null", row: 5}, {id: 12, color: "null", row: 5}, {id: 13, color: "null", row: 5}, {id: 14, color: "null", row: 5},
+				{id: 15, color: "null", row: 4}, {id: 16, color: "null", row: 4}, {id: 17, color: "null", row: 4}, {id: 18, color: "null", row: 4}, {id: 19, color: "null", row: 4}, {id: 21, color: "null", row: 4}, {id: 21, color: "null", row: 4},
+				{id: 22, color: "null", row: 3}, {id: 23, color: "null", row: 3}, {id: 24, color: "null", row: 3}, {id: 25, color: "null", row: 3}, {id: 26, color: "null", row: 3}, {id: 27, color: "null", row: 3}, {id: 28, color: "null", row: 3},
+				{id: 29, color: "null", row: 2}, {id: 30, color: "null", row: 2}, {id: 31, color: "null", row: 2}, {id: 32, color: "null", row: 2}, {id: 33, color: "null", row: 2}, {id: 34, color: "null", row: 2}, {id: 35, color: "null", row: 2},
+				{id: 36, color: "null", row: 1}, {id: 37, color: "null", row: 1}, {id: 38, color: "null", row: 1}, {id: 39, color: "null", row: 1}, {id: 40, color: "null", row: 1}, {id: 41, color: "null", row: 1}, {id: 42, color: "null", row: 1}
+			],
+			buttonArray: [
+				{column: 1}, {column: 2}, {column: 3}, {column: 4}, {column: 5}, {column: 6}, {column: 7}
 			]
         };
 	},
 	methods: {
-        
-            
+        addChip(column) {
+			let bottomId = this.findBottomId(column);
+			while(this.grid[bottomId - 1].color != "null"){
+				bottomId -= 7;
+				if(bottomId < 1){
+					console.log("Row is full!");
+					break;
+				}
+			}
+			this.grid[bottomId - 1].color = "black";
+		},
+		findBottomId(column) {
+			return column + (7 * 5);
+		},  
     },
 	computed: {},
 	mounted() {
